ece642a1021691e11bac317b84f219ce592859c9
dragon_123 <dragon_123@126.com>
2017-08-24 22:18:13 +0800

    change_5

--- arr1.php
+++ arr1.php
@@ -14,13 +14,14 @@ $aa=date("Y-m-d",$a);
 echo date("Y-m-d",$a)."<br>";
 $b='1503581974';
 echo date("Y-m-d",$b)."<br>";
-$date_ar=['1103481111','1503581974'];
-var_dump($date_ar);
+
+//$date_ar=['1103481111','1503581974'];
+//var_dump($date_ar);
 //echo   date("Y-m-d",strtotime("+1 month",strtotime("2012-02-04")));
 $d=[];
-for($i=0;$i<200;$i++) {
+for($i=0;$i<10;$i++) {
 	$c=date("Y-m-d",strtotime("+".$i." month",strtotime($aa)));
 	array_push($d,$c);
 }
-//var_dump($d);
+print_r($d);
 //echo   date("Y-m-d",strtotime("+1 month",strtotime($aa)));

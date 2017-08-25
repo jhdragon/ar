7d011ce22c7a21808926285c64cc2c9e6e67cdd7
dragon_123 <dragon_123@126.com>
2017-08-25 15:31:05 +0800

    change_10

--- arr1.php
+++ arr1.php
@@ -5,33 +5,41 @@
 或者这样处理，压数组前判 断一下年份是否大于2020年，如果大于则不压数组了
 **/
 
-echo strtotime("2009-10-21 16:00:10")."<br>";
-echo strtotime("2009-10-21")."<br>";
+//echo strtotime("2009-10-21 16:00:10")."<br>";
+//echo strtotime("2009-10-21")."<br>";
 //日期转成时代戳,然后压数组
 
 $a='1103481111';
 $aa=date("Y-m-d",$a);
-echo date("Y-m-d",$a)."<br>";
+//echo date("Y-m-d",$a)."<br>";
 $b='1503581974';
-echo date("Y-m-d",$b)."<br>";
+//echo date("Y-m-d",$b)."<br>";
 
 //$date_ar=['1103481111','1503581974'];
 //var_dump($date_ar);
 //echo   date("Y-m-d",strtotime("+1 month",strtotime("2012-02-04")));
-$d=[];
-for($i=0;$i<50;$i++) {
+$d=['check'=>[],'view'=>[]];
+for($i=0;$i<10;$i++) {
 	if($i==0){
 		array_push($d,$a);
 	}else {
 		$c=date("Y-m-d",strtotime("+".$i." month",strtotime($aa)));
 		$e=strtotime($c);
-		array_push($d,$e);
+		array_push($d['check'],$e);
 	}
 }
-print_r($d);
-$d_l=count($d);
+
+$d_l=count($d['check']);
 for($x=0;$x<$d_l;$x++) {
-	$cc=date("Y-m-d",$d[$x]);
-	echo $cc."<br>";
+	$cc=date("Y-m-d",$d['check'][$x]);
+	//echo $cc."<br>";
+	array_push($d['view'],$cc);
 }
-//echo   date("Y-m-d",strtotime("+1 month",strtotime($aa)));
+print_r($d);
+?>
+
+<select>
+<?php foreach($d['view'] as $v=>$k){ ?>
+	<option value="<?php echo $v;?>"><?php echo $k;?></option>
+<?php }?>	
+</select>
